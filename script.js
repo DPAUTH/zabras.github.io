95dbd772ade68da89ac8934f5abd826f0d65d3fb
DPAUTH <dimpapser@csd.auth.gr>
2023-06-30 14:46:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const jokeEL = document.getElementById('joke');
+const btn = document.getElementById('jokeBtn');
+
+
+btn.addEventListener('click', getJoke)
+getJoke();
+// using ASYNC/AWAIT
+async function getJoke(){
+    
+    const config = {
+        headers:{
+            'Accept': 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json() 
+    jokeEL.innerHTML = data.joke;
+}
+
+
+
+
+
